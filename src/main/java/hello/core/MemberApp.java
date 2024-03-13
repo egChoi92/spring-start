ceb7a0ae014922924921e7651f3aca3e610c7c21
egChoi <egChoi92@gmail.com>
2024-03-13 15:40:03 +0900

    3. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용
    - 순수 자바 코드로 작성

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -2,11 +2,14 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberServiceImpl memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+//        MemberServiceImpl memberService = new MemberServiceImpl();
+        MemberService memberService = appConfig.memberService();
         Member memberA = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(memberA);
 
